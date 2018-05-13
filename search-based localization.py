4c96c5607648c146e7fc99dcd5bbaa825c6887e2
Hossam Elbahrawy <hossam_elbahrawy@hotmail.com>
2018-05-13 14:27:28 +0200

    Sending every action to the micro

--- search-based localization.py
+++ search-based localization.py
@@ -5,129 +5,136 @@ import pickle
 from random import randint
 from Astar import aStarSearch
 from SearchProblem import SearchProblem
-
+import serial
+import time
+ser=serial.Serial('/dev/ttyAMA0',baudrate=9600, bytesize=serial.EIGHTBITS,timeout=1,stopbits=1)
 
 class localization:
-    
+
     def __init__(self, start, obstacles, map_d, obstacles_num):
-        
+
         self.start = start
         self.obstacles = obstacles
         self.dim = map_d
         self.num = obstacles_num
 
     def search_and_localize(self):
-    
+
         goal = self.get_random_goal()
-        
+
 
         problem = SearchProblem(self.start, goal, self.obstacles, self.dim)
-            
+
         path = aStarSearch(problem)
-        
-        
+
+
         i = 0
-       
+
         position = self.start
         action = path[i]
-       
-             
+
+
         while(1):
-            
+
             distance_west = distanceMeasurement(triggerList[0], echoList[0], 0)
             distance_north = distanceMeasurement(triggerList[1], echoList[1], 1)
             distance_east = distanceMeasurement(triggerList[2], echoList[2], 2)
-                
+
             if (distance_west < 10 and action == "W"):
-                
+
                  increment = self.get_obstacle_pos(action)
                  obstacle = tuple(map(operator.add, position, increment))
-                 
+
                  self.add_obstacles(obstacle)
-                 
+
                  self.start = position
-                 
+
                  break
-                
+
             elif(distance_north < 10 and action == "N"):
-                
+
                  increment = self.get_obstacle_pos(action)
                  obstacle = tuple(map(operator.add, position, increment))
-                 
+
                  self.add_obstacles(obstacle)
-                 
+
                  self.start = position
-                 
+
                  break
-                
-                
+
+
             elif(distance_east < 10 and action == "E"):
-                
+
                  increment = self.get_obstacle_pos(action)
                  obstacle = tuple(map(operator.add, position, increment))
-                 
+
                  self.add_obstacles(obstacle)
-                 
+
                  self.start = position
-                 
+
                  break
-                 
-                
+
+
             else:
-                
+
                 #send action to MC
+                print(action)
+                ser.write(action)
+                data=0
+                while data !='1':
+					data=ser.read(10)
                 increment = self.get_obstacle_pos(action)
                 position = tuple(map(operator.add, position, increment))
                 i += 1
-                
+
                 self.start = position
-                
+
                 if(i == len(path)):
                     break
-                
+
                 action = path[i]
-                
-                
-            
-            
-    
+
+
+
+
+
     def get_obstacle_pos(self, direction):
-        
+
         return{
                 "N": (0, 1),
                 "S": (0, -1),
                 "E": (1, 0),
                 "W": (-1, 0)}.get(direction, (0, 0))
-        
-                
+
+
     def add_obstacles(self, obstacle):
-        
+
         self.obstacles.add(obstacle)
-        
-        
+
+
     def get_obstacles(self):
-        
+
         return self.obstacles
-        
-        
+
+
     def get_random_goal(self):
-        
+
         while(1):
-            
+
             goal = tuple((randint(0, self.dim - 1), randint(0, self.dim - 1)))
-            
+
             if(goal != self.start and goal not in self.obstacles):
                 return goal
-            
-        
+
+
     def stop(self):
-        
+
         if(self.num == len(self.obstacles)):
             return True
-        
+
         return False
-        
-                
+
+
 start = (0, 0)
 obstacles = set(())
 map_d = 4               #4 x 4
@@ -141,16 +148,16 @@ robot = localization(start, obstacles, map_d, obstacles_num)
 
 
 while(10):
-    
+
     robot.search_and_localize()
-    
+
     #if(robot.stop()):
-        
+
         #obstacles = robot.get_obstacles()
         #break
-        
 
-#obstacles = [(1, 2), (2, 3)] 
+
+#obstacles = [(1, 2), (2, 3)]
 
 obstacles = robot.get_obstacles()
 
@@ -158,38 +165,9 @@ grid = np.zeros((map_d, map_d))
 
 for i in range(len(obstacles)):
     grid[obstacles[i]] = 1
-    
+
 print(grid)
 
-    
+
 with open("obstacles", "wb") as fb:
     pickle.dump(obstacles, fb)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-            
